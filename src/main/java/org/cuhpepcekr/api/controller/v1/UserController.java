dab3340fab11a48933f59d029c98b169affc7d3f
cuhpepcekr <lintogm@gmail.com>
2020-07-04 23:52:39 +0900

    Swagger API 문서 자동화.

--- src/main/java/org/cuhpepcekr/api/controller/v1/UserController.java
+++ src/main/java/org/cuhpepcekr/api/controller/v1/UserController.java
@@ -1,31 +1,36 @@
 package org.cuhpepcekr.api.controller.v1;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.cuhpepcekr.api.entity.User;
 import org.cuhpepcekr.api.repo.UserJpaRepo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"1. User"})
 @RequiredArgsConstructor
-@RestController // 결과값을 JSON으로 출력합니다.
+@RestController
 @RequestMapping(value = "/v1")
 public class UserController {
+
     private final UserJpaRepo userJpaRepo;
 
+    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다")
     @GetMapping(value = "/user")
     public List<User> findAllUser() {
         return userJpaRepo.findAll();
     }
 
+    @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
     @PostMapping(value = "/user")
-    public User save() {
+    public User save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
+                     @ApiParam(value = "회원이름", required = true) @RequestParam String name) {
         User user = User.builder()
-                .uid("yumi@naver.com")
-                .name("유미")
+                .uid(uid)
+                .name(name)
                 .build();
         return userJpaRepo.save(user);
     }
