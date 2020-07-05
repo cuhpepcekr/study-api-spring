e21b7e7bc3978f17c3c09411b2c11bf788c47874
cuhpepcekr <lintogm@gmail.com>
2020-07-05 19:59:06 +0900

    MessageSource를 이용한 Exception 처리.

--- src/main/java/org/cuhpepcekr/api/advice/ExceptionAdvice.java
+++ src/main/java/org/cuhpepcekr/api/advice/ExceptionAdvice.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.cuhpepcekr.api.advice.exception.CUserNotFoundException;
 import org.cuhpepcekr.api.model.response.CommonResult;
 import org.cuhpepcekr.api.service.ResponseService;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,20 +15,33 @@ import javax.servlet.http.HttpServletRequest;
 
 @RequiredArgsConstructor
 @RestControllerAdvice
-// @RestControllerAdvice(basePackages = “com.rest.api”) 특정 패키지 하위의 Controller에만 로직이 적용되게도 할 수 있습니다.
 public class ExceptionAdvice {
 
     private final ResponseService responseService;
 
+    private final MessageSource messageSource;
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     protected CommonResult defaultException(HttpServletRequest request, Exception e) {
-        return responseService.getFailResult();
+        // 예외 처리의 메시지를 MessageSource에서 가져오도록 수정
+        return responseService.getFailResult(Integer.valueOf(getMessage("unKnown.code")), getMessage("unKnown.msg"));
     }
 
     @ExceptionHandler(CUserNotFoundException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     protected CommonResult userNotFoundException(HttpServletRequest request, CUserNotFoundException e) {
-        return responseService.getFailResult();
+        // 예외 처리의 메시지를 MessageSource에서 가져오도록 수정
+        return responseService.getFailResult(Integer.valueOf(getMessage("userNotFound.code")), getMessage("userNotFound.msg"));
+    }
+
+    // code정보에 해당하는 메시지를 조회합니다.
+    private String getMessage(String code) {
+        return getMessage(code, null);
+    }
+
+    // code정보, 추가 argument로 현재 locale에 맞는 메시지를 조회합니다.
+    private String getMessage(String code, Object[] args) {
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
     }
 }
